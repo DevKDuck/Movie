062581a0518091ef9260890fff72d1526151c95c
DevKDuck <loading95@naver.com>
2024-03-07 14:56:57 +0900

    수정 기능 구현

--- Movie/src/Board/Board.java
+++ Movie/src/Board/Board.java
@@ -1,7 +1,14 @@
 package Board;
 
-import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Board {
 	private int bno;
 	private String btitle;
@@ -9,46 +16,80 @@ public class Board {
 	private String bwriter;
 	private String bdate;
 	
-	
-	public Board(int bno, String btitle, String bcontent, String bwriter, String bdate) {
-		this.bno = bno;
-		this.btitle = btitle;
-		this.bcontent = bcontent;
-		this.bwriter = bwriter;
-		this.bdate = bdate;
+	public Board(String title, String content, String writer) {
+		this(0, title, content, writer);
 	}
 	
+	public Board(int bno, String title, String content, String writer) {
+		this(bno, title, content, writer, "");
+	}
+
 	
+	public Board(int bno2, String title, String content, String writer, String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void print() {
+		System.out.printf("%-6s%-12s%-16s%-40s \n"
+				, bno
+				, bwriter
+				, bdate
+				, btitle);
+	}
 	
-	
+	public void printDetail() {
+		System.out.println("번호 : " + bno);
+		System.out.println("작성자 : " + bwriter);
+		System.out.println("작성일자 : " + bdate);
+		System.out.println("제목 : " + btitle);
+		System.out.println("내용 : " + bcontent);
+	}
+
 	public int getBno() {
 		return bno;
 	}
+
 	public void setBno(int bno) {
 		this.bno = bno;
 	}
+
 	public String getBtitle() {
 		return btitle;
 	}
+
 	public void setBtitle(String btitle) {
 		this.btitle = btitle;
 	}
+
 	public String getBcontent() {
 		return bcontent;
 	}
+
 	public void setBcontent(String bcontent) {
 		this.bcontent = bcontent;
 	}
+
 	public String getBwriter() {
 		return bwriter;
 	}
+
 	public void setBwriter(String bwriter) {
 		this.bwriter = bwriter;
 	}
+
 	public String getBdate() {
 		return bdate;
 	}
+
 	public void setBdate(String bdate) {
 		this.bdate = bdate;
 	}
+
+
+
+
+
+
+
+	
 }
